7e33a199cfd983835f93393e0131bfbac7a4fe84
bloodchillin <tsu_dake@hotmail.com>
2024-03-15 22:38:01 +1000

    iMap 1.4.0
    -- Custom Markers introduced
    -- Beginning of Waypoints/Poles
    -- Moved images to own folder for clarity purposes

--- poi.js
+++ poi.js
@@ -9,3 +9,57 @@ var xppoles = {
             "id": 2}
     ]
 };
+
+var chests = {
+    "type": "FeatureCollection",
+    "features": [
+        {"geometry": {"type": "Point","coordinates": [274,143]},
+            "type": "Feature","properties": {"popupContent": "TEST 1"},
+            "id": 1},
+		{"geometry": {"type": "Point","coordinates": [522,239]},
+            "type": "Feature","properties": {"popupContent": "TEST 2"},
+            "id": 2}
+    ]
+};
+
+var Waypoints = {
+    "type": "FeatureCollection",
+    "features": [
+        {"geometry": {"type": "Point","coordinates": [1060,984]},
+            "type": "Feature","properties": {"popupContent": "Lynbore, Tavern"},
+            "id": 1},
+		{"geometry": {"type": "Point","coordinates": [1572,1140]},
+            "type": "Feature","properties": {"popupContent": "Lynbore, East"},
+            "id": 2},
+		{"geometry": {"type": "Point","coordinates": [1884,1468]},
+            "type": "Feature","properties": {"popupContent": "Aston, South"},
+            "id": 3},
+		{"geometry": {"type": "Point","coordinates": [1964,1904]},
+            "type": "Feature","properties": {"popupContent": "Aston, Crossroads"},
+            "id": 4},
+		{"geometry": {"type": "Point","coordinates": [1596,1896]},
+            "type": "Feature","properties": {"popupContent": "Aston, West"},
+            "id": 5},
+		{"geometry": {"type": "Point","coordinates": [2424,1896]},
+            "type": "Feature","properties": {"popupContent": "Aston, East"},
+            "id": 6},
+		{"geometry": {"type": "Point","coordinates": [1980,912]},
+            "type": "Feature","properties": {"popupContent": "Forgotten Canyon"},
+            "id": 7},
+		{"geometry": {"type": "Point","coordinates": [2844,2660]},
+            "type": "Feature","properties": {"popupContent": "Lizard Temple, North"},
+            "id": 8},
+		{"geometry": {"type": "Point","coordinates": [2964,1464]},
+            "type": "Feature","properties": {"popupContent": "Temple of Osiris"},
+            "id": 9},
+		{"geometry": {"type": "Point","coordinates": [2048,2048]},
+            "type": "Feature","properties": {"popupContent": "Neiseer, West"},
+            "id": 10},
+		{"geometry": {"type": "Point","coordinates": [1596,1896]},
+            "type": "Feature","properties": {"popupContent": "Aston, West"},
+            "id": 11},
+		{"geometry": {"type": "Point","coordinates": [2424,1896]},
+            "type": "Feature","properties": {"popupContent": "Aston, East"},
+            "id": 12},
+    ]
+};
