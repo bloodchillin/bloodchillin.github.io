cc7dd5a3df266b858d16422ec95da09d960b51f7
bloodchillin <tsu_dake@hotmail.com>
2024-03-15 19:36:05 +1000

    iMap 1.3.0
    first implementation of geojson importing of POIs
    
    first iteration of layer controls

---
+++ poi.js
@@ -0,0 +1,11 @@
+var xppoles = {
+    "type": "FeatureCollection",
+    "features": [
+        {"geometry": {"type": "Point","coordinates": [274,143]},
+            "type": "Feature","properties": {"popupContent": "TEST 1"},
+            "id": 1},
+		{"geometry": {"type": "Point","coordinates": [522,239]},
+            "type": "Feature","properties": {"popupContent": "TEST 2"},
+            "id": 2}
+    ]
+};
