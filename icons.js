41c0ba2da0a2a1d43c80ea751192fcf06545bcdc
bloodchillin <tsu_dake@hotmail.com>
2024-03-19 22:53:17 +1000

    update
    New icons
    
    Additional markers

---
+++ icons.js
@@ -0,0 +1,21 @@
+// Define custom markers
+
+	const poleIcon = new L.Icon({
+		iconUrl: './img/polemarker.png',
+		iconSize: [25,49]
+		});
+		
+	const wpIcon = new L.Icon({
+		iconUrl: './img/wpmarker.png',
+		iconSize: [32,64]
+	});
+	
+	const qIcon = new L.Icon({
+		iconUrl: './img/qmarker.png',
+		iconSize: [24,24]
+	});
+	
+	const shopIcon = new L.Icon({
+		iconUrl: './img/shopmarker.png',
+		iconSize:[20,20]		
+	});
--- poi.js
+++ poi.js
@@ -88,13 +88,22 @@ var Waypoints = {
     ]
 };
 
-var npc = {
-    "type": "FeatureCollection",
-    "features": [
+var astonshop = {
+	"type": "FeatureCollection",
+	"features": [
+	
         {"geometry": {"type": "Point","coordinates": [1906.25,2090.25]},
 			"type": "Feature","properties": {"name": "Ronald"}},
 		{"geometry": {"type": "Point","coordinates": [1906.25,2076.25]},
 			"type": "Feature","properties": {"name": "Gani"}},
+	
+	]
+};
+
+var astonquest = {
+    "type": "FeatureCollection",
+    "features": [
+
 		{"geometry": {"type": "Point","coordinates": [1898.25,2025.75]},
 			"type": "Feature","properties": {"name": "Manager"}},
 		{"geometry": {"type": "Point","coordinates": [1897.75,1881.75]},
@@ -104,7 +113,41 @@ var npc = {
 		{"geometry": {"type": "Point","coordinates": [1897.75,1629.75]},
             "type": "Feature","properties": {"name": "Elric"}},
 		{"geometry": {"type": "Point","coordinates": [1877.5,1597.5]},
-            "type": "Feature","properties": {"name": "Cherri"}}
+            "type": "Feature","properties": {"name": "Cherri"}},
+		{"geometry": {"type": "Point","coordinates": [1878.5,1510.25]},
+            "type": "Feature","properties": {"name": "Gene"}},
+		{"geometry": {"type": "Point","coordinates": [1790.25,1505.25]},
+            "type": "Feature","properties": {"name": "Robin"}},
+		{"geometry": {"type": "Point","coordinates": [1745.5,1505.75]},
+            "type": "Feature","properties": {"name": "Monica"}},
+		{"geometry": {"type": "Point","coordinates": [1722.25,1506.25]},
+            "type": "Feature","properties": {"name": "Danica"}},
+		{"geometry": {"type": "Point","coordinates": [1750.5,1586.5]},
+            "type": "Feature","properties": {"name": "Jamie"}},
+		{"geometry": {"type": "Point","coordinates": [1737.75,1657.5]},
+            "type": "Feature","properties": {"name": "Terri"}},	
+		{"geometry": {"type": "Point","coordinates": [1737,1697.5]},
+            "type": "Feature","properties": {"name": "Ellis"}},
+		{"geometry": {"type": "Point","coordinates": [1658,1722]},
+            "type": "Feature","properties": {"name": "Sierra"}},
+		{"geometry": {"type": "Point","coordinates": [1745.75,1742.25]},
+            "type": "Feature","properties": {"name": "Roger"}},
+		{"geometry": {"type": "Point","coordinates": [1737.5,1801.5]},
+            "type": "Feature","properties": {"name": "Boris"}},
+		{"geometry": {"type": "Point","coordinates": [1737.5,1837.75]},
+            "type": "Feature","properties": {"name": "Kaleigh"}},
+		{"geometry": {"type": "Point","coordinates": [1641.75,1898.75]},
+            "type": "Feature","properties": {"name": "Tactician"}},
+		{"geometry": {"type": "Point","coordinates": [1665.5,1986.25]},
+            "type": "Feature","properties": {"name": "Western Watch"}},
+		{"geometry": {"type": "Point","coordinates": [1745.5,1990.25]},
+            "type": "Feature","properties": {"name": "Shera"}},
+		{"geometry": {"type": "Point","coordinates": [1745.5,2078]},
+            "type": "Feature","properties": {"name": "Iggy"}},
+		{"geometry": {"type": "Point","coordinates": [1781.5,1985.5]},
+            "type": "Feature","properties": {"name": "Oscar"}},
+		{"geometry": {"type": "Point","coordinates": [1874.25,1965.75]},
+            "type": "Feature","properties": {"name": "Roxie"}}				
     ]
 };
 
